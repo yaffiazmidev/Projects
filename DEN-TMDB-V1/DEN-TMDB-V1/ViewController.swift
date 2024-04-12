6ff32d25e742c7e28d6c890d596b61481a61ab74
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-04-13 01:05:51 +0700

    Create a new project
    Create a new project

---
+++ DEN-TMDB-V1/DEN-TMDB-V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DEN-TMDB-V1
+//
+//  Created by DENAZMI on 13/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

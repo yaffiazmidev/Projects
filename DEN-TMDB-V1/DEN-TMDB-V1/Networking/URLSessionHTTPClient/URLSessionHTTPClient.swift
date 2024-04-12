679863df9f3e3347c7268acd8d4105e569965e24
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-04-13 01:34:59 +0700

    Create URLSessionHTTPClient

---
+++ DEN-TMDB-V1/DEN-TMDB-V1/Networking/URLSessionHTTPClient/URLSessionHTTPClient.swift
@@ -0,0 +1,47 @@
+//
+//  URLSessionHTTPClient.swift
+//  DEN-TMDB-V1
+//
+//  Created by DENAZMI on 13/04/24.
+//
+
+import Foundation
+
+final class URLSessionHTTPClient: HTTPClient {
+    
+    typealias Result = HTTPClient.Result
+    
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func request(from request: URLRequest, completion: @escaping (Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        
+        task.resume()
+        
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}

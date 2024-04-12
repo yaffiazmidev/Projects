92155bcecdd39ef445adad9df2aed2d91c143e5e
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-04-13 01:55:36 +0700

    Create Home controller

---
+++ DEN-TMDB-V1/DEN-TMDB-V1/Features/Home/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  DEN-TMDB-V1
+//
+//  Created by DENAZMI on 13/04/24.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

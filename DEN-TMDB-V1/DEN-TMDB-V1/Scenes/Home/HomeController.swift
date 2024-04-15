c1454ef8a703c6e3b0506f473b4683043e408755
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-04-16 01:51:59 +0700

    Create Home VIP arc

--- DEN-TMDB-V1/DEN-TMDB-V1/Scenes/Home/HomeController.swift
+++ DEN-TMDB-V1/DEN-TMDB-V1/Scenes/Home/HomeController.swift
@@ -7,9 +7,30 @@
 
 import UIKit
 
+protocol IHomeController: AnyObject {
+}
+
 class HomeController: UIViewController {
+    
+    private let interactor: IHomeInteractor
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    init(interactor: IHomeInteractor) {
+        self.interactor = interactor
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
+
+extension HomeController: IHomeController {
+}
+
+
+

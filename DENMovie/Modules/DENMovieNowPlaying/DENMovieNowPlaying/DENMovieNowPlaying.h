d93ad23ec2cf3c2f1194e24d2684ee1c8a00eb41
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-05-20 20:01:31 +0700

    Create Now Playing Module

---
+++ DENMovie/Modules/DENMovieNowPlaying/DENMovieNowPlaying/DENMovieNowPlaying.h
@@ -0,0 +1,18 @@
+//
+//  DENMovieNowPlaying.h
+//  DENMovieNowPlaying
+//
+//  Created by DENAZMI on 20/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DENMovieNowPlaying.
+FOUNDATION_EXPORT double DENMovieNowPlayingVersionNumber;
+
+//! Project version string for DENMovieNowPlaying.
+FOUNDATION_EXPORT const unsigned char DENMovieNowPlayingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DENMovieNowPlaying/PublicHeader.h>
+
+

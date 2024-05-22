514464374ccbdc6f32990ee9b2bf80f32c0212dc
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-05-22 10:51:19 +0700

    Configure Modules

---
+++ DENMovie/Modules/DENMovieNowPlaying/DENMovieNowPlayingiOS/Sources/Controllers/NowPlayingController.swift
@@ -0,0 +1,16 @@
+//
+//  NowPlayingController.swift
+//  DENMovieNowPlayingiOS
+//
+//  Created by DENAZMI on 22/05/24.
+//
+
+import UIKit
+
+public class NowPlayingController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}

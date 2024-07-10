2bc85ff7efa9ff33fb83a96e2f4a523a8c20eac2
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-07-11 03:04:08 +0700

    Create Networking module

---
+++ DENMov/Modules/DENMovNetworking/DENMovNetworking/DENMovNetworking.h
@@ -0,0 +1,18 @@
+//
+//  DENMovNetworking.h
+//  DENMovNetworking
+//
+//  Created by DENAZMI on 11/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DENMovNetworking.
+FOUNDATION_EXPORT double DENMovNetworkingVersionNumber;
+
+//! Project version string for DENMovNetworking.
+FOUNDATION_EXPORT const unsigned char DENMovNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DENMovNetworking/PublicHeader.h>
+
+

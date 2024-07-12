51edd8ac592b4e5b2c0493bc9092c1c532695aa3
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-07-12 08:52:10 +0700

    DENMovNowPlayingiOS

---
+++ DENMov/Modules/DENMovNowPlaying/DENMovNowPlayingiOS/Sources/Controllers/NowPlayingController.swift
@@ -0,0 +1,15 @@
+//
+//  NowPlayingController.swift
+//  DENMovNowPlayingiOS
+//
+//  Created by DENAZMI on 12/07/24.
+//
+
+import UIKit
+
+public class NowPlayingController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
